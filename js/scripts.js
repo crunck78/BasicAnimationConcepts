e29d285b5f623cd158ac8411f66d1460bbad58f2
Mihai-Andrei Neacsu <crunck78@googlemail.com>
2021-02-15 22:19:28 +0100

    save

--- js/scripts.js
+++ js/scripts.js
@@ -1,87 +1,131 @@
-const RIGHT = 0;
-const LEFT = 1;
-const GRAVITY = 98.1;
-let cnv = document.getElementById("cnv");
+/**
+ * Reference to a div element used to log infos  out.
+ */
+const log = document.getElementById("log");
+/**
+ * Reference to the canvas element.
+ */
+const cnv = document.getElementById("cnv");
 cnv.width = window.innerWidth;
 cnv.height = window.innerWidth * 0.75;
 cnv.style.border = "3px solid black";
-let ctx = cnv.getContext("2d");
+/**
+ * 2d rendering context to draw in the canvas.
+ * @constant {CanvasRenderingContext2D}
+ */
+const ctx = cnv.getContext("2d");
+
 const GROUND_POS = cnv.height - 150;
+const RIGHT = 0; 
+const LEFT = 1;
+const GRAVITY = 98.1 * 10;
 
-let log = document.getElementById("log");
-let startDraw;
-let startUpdate;
 let startJump;
-let MOVEMENT_SPEED = 20;
+let startThrow;
+let startDraw;
 
-let player = createPlayer(20, GROUND_POS, 150, 100);
-let obj = createObj(cnv.width - 200, GROUND_POS, 150, 100);
+let obj = createObj(cnv.width - 200, GROUND_POS - 100, 150, 100);
+let player = createPlayer(20, GROUND_POS - 100, 150, 100);
+player.bottle.initY = player.y;
 
-function drawLoop(timestamp){
-	if (startDraw === undefined)
-    	startDraw = timestamp;
-  	const elapsed = timestamp - startDraw;
+/**
+ * Draws every frame of the game, all draw calles should be issue inside it.
+ */
+function drawLoop(timeStamp) {
+	if(startDraw === undefined){
+		startDraw = timeStamp;
+	}
+	
 	clearCanvas();
+	drawGroundLine();
 	draw(obj);
 	draw(player);
+	if(player.throwInProgress){
+		draw(player.bottle);
+		let bottle = new Image();
+		const elapseBottleChange = Math.trunc((timeStamp - startDraw) / 60);
+		bottle.src = ` C:/Users/scoo_/Documents/dev/GitHub/eu/el-pollo-loco-v3/img/bottle-throw/bottleThrow_${elapseBottleChange % 4}.png`;
+		if(bottle.complete)
+			ctx.drawImage(bottle, player.bottle.x, player.bottle.y, bottle.width * 0.2, bottle.height * 0.2);
+	}
 	requestAnimationFrame(drawLoop);
 }
 
-function updateLoop(timestamp){
-	if(player.isMovingLeft){
-		obj.x += MOVEMENT_SPEED * obj.distance;
-	}
-	
-	if(player.isMovingRight){
-		obj.x -= MOVEMENT_SPEED * obj.distance;
-	}
-	
+/**
+ * Temporary function used for testing game functionalities like movements, collisions, etc
+ */
+function updateLoop() {
 	obj.x -= obj.movementSpeed * obj.distance;
-	
-	//console.log(isColliding(player, obj));
-	//console.log(player.requestJump);
-	
 	requestAnimationFrame(updateLoop);
 }
 
-//function updatePlayer(timeStamp){
-// 	//TODO
-//	let timeStampInSec = timeStamp / 1000;
-//	player.y = (player.jumpVelocity * timeStampInSec) + (0.5 * GRAVITY * Math.pow(timeStampInSec, 2)) + GROUND_POS;
-//	log.innerHTML = `${player.y}<br>${Math.trunc(timeStampInSec)}<br>${reqId}`;
-//	if( player.y <= GROUND_POS && timeStampInSec > 0)
-//		requestAnimationFrame(updatePlayer);
-//}
+/**
+ * Invoked if Player presses down the left arrow. Cancelled if Player stops pressing
+ */
+function moveLeft(){
+	obj.x += player.movementSpeed * obj.distance;
+	player.requestMoveLeft = requestAnimationFrame(moveLeft);
+}
 
-function updatePlayer(timeStamp){
-	
-	if(startJump === undefined){
-		//log.innerHTML += `<br>FIRST JUMP START<br>`;
-		startJump = timeStamp;
-	}else if(player.y <= GROUND_POS){
-		//log.innerHTML = `<br>PERFORMING JUMP<br>`;
-	}else{
-		//log.innerHTML += `<br>PERFORMING NEXT JUMP<br>`;
-		startJump = timeStamp;
-	}
-	const elapse = (timeStamp - startJump) / 1000;
-	//log.innerHTML += `<br>${player.y}<br>${elapse}<br>${startJump}<br>`;
-	player.y = (player.jumpVelocity * elapse) + (0.5 * GRAVITY * Math.pow(elapse, 2)) + GROUND_POS;
-	//log.innerHTML += `<br>${player.y}<br>${elapse}<br>${startJump}<br>`;
-	if(player.y <= GROUND_POS){
-		//log.innerHTML += `<br>STILL IN THERE AIR<br>`;
-		requestAnimationFrame(updatePlayer);
-	}
-	else{
-		log.innerHTML = `<br>LANDED ON GROUND POS<br>`;
-		//player.y = GROUND_POS;
+/**
+ * Invoked if Player presses down the right arrow. Cancelled if Player stops pressing
+ */
+function moveRight(){
+	obj.x -= player.movementSpeed * obj.distance;
+	player.requestMoveRight = requestAnimationFrame(moveRight);
+}
+
+/**
+ * A function of Time that Calculates the change in vertical position of players's jumping motion.
+ * @param {number} timeStamp - automatically passed, indicating the precise time requestAnimationFrame() was called.
+ */
+function jump(timeStamp) {
+	if (startJump === undefined || !player.jumpInProgess)// jump start
+		startJump = timeStamp;// set timer to 0
+	const elapse = (timeStamp - startJump) / 1000; //SECONDS
+	player.y = (player.jumpVelocity * elapse) + (0.5 * GRAVITY * Math.pow(elapse, 2)) + player.groundPos;// y= v*sin(angle)*deltaTime + 1/2*accel(Constant)*t^2 
+	if ((player.y == player.groundPos && elapse == 0) || (elapse > 0 && player.y < player.groundPos)) {// first animation frame or not last animation frame
+		player.jumpInProgess = true;
+		requestAnimationFrame(jump);
+	} else { // jump finnish
+		player.jumpInProgess = false;
+		player.y = player.groundPos; //adjust to same altitude
 	}
 }
 
-function createObj(xPos, yPos, width, height){
-	return{
+/**
+ * A function of Time that Calculates the change in positions of players's bottle's throw motion.
+ * @param {number} timeStamp - automatically passed, indicating the precise time requestAnimationFrame() was called.
+ */
+function throwBottle(timeStamp){
+	if (startThrow === undefined || !player.throwInProgress)
+		startThrow = timeStamp; //throw start animation
+	const elapse = (timeStamp - startThrow) / 1000; //SECONDS
+	player.bottle.x = player.x + (-player.throwVelocity * Math.cos(player.throwAngle) * elapse); // x = xinit + (vinit * cos(angle)*changeInTime)
+	player.bottle.y = (player.throwVelocity * Math.sin(player.throwAngle) * elapse) + (0.5 * GRAVITY * Math.pow(elapse, 2)) + player.bottle.initY; // y= v*sin(angle)*deltaTime + 1/2*accel(Constant)*t^2 + yinit
+	if ((player.bottle.y == player.bottle.initY && elapse == 0) || (elapse > 0 && player.bottle.y < player.bottle.groundPos)) { // if first animation frame, or not last aniamtion frame
+		player.throwInProgress = true;
+		requestAnimationFrame(throwBottle);
+	} else {
+		player.throwInProgress = false;
+		player.bottle.y = player.bottle.groundPos; // throw hit the ground , adjust to same altitude
+	}
+}
+
+
+/**
+ * Returns an object representing an item. Instance can be used as an argument to draw(elm) calls to visualize the item as a rectangle  
+ * @param {number} xPos - cooordinate x on canvas
+ * @param {number} yPos - coordinate y on canvas
+ * @param {number} width - width of the instance
+ * @param {number} height - height of the instance
+ * @returns {{}} - object structure of a player
+ */
+function createObj(xPos, yPos, width, height) {
+	return {
 		x: xPos,
 		y: yPos,
+		groundPos: GROUND_POS - height,
 		width: width,
 		height: height,
 		movementSpeed: 1,
@@ -89,109 +133,148 @@ function createObj(xPos, yPos, width, height){
 	};
 }
 
-function createPlayer(xPos, yPos, width, height){
-	return{
+/**
+ * Returns an object representing the player. Instance can be used as an argument to draw(elm) calls to visualize player as a rectangle  
+ * @param {number} xPos - cooordinate x on canvas
+ * @param {number} yPos - coordinate y on canvas
+ * @param {number} width - width of the instance
+ * @param {number} height - height of the instance
+ * @returns {{}} - object structure of a player
+ */
+function createPlayer(xPos, yPos, width, height) {
+	return {
 		x: xPos,
 		y: yPos,
+		groundPos: GROUND_POS - height,
 		width: width,
 		height: height,
 		isMovingLeft: false,
 		isMovingRight: false,
-		isJumping: false,
 		direction: RIGHT,
-		allowJump: false,
-		jumpVelocity: -200,
-		jumpRequestTime: 0
+		movementSpeed: 5,
+		requestMoveRight: 0,
+		requestMoveLeft: 0,
+		jumpVelocity: -98.1 * 10,
+		throwVelocity: -98.1 * 10,
+		throwAngle: 0.78539816, //Radians -> 45 Degrees
+		jumpInProgess: false,
+		throwInProgress: false,
+		bottle: createObj(this.xPos, this.yPos, 50, 50)
+		// throwTime: (2 * this.jumpVelocity * Math.sin(this.throwVelocity)) / GRAVITY,
+		// jumpTime: (2 * this.jumpVelocity) / GRAVITY
 	};
 }
 
-function clearCanvas(){
+/**
+ * Used to clear The Canvas on repainting, should be called 1st, before any other draw calls.
+ */
+function clearCanvas() {
 	ctx.beginPath();
 	ctx.rect(0, 0, cnv.width, cnv.height);
 	ctx.fillStyle = "white";
 	ctx.fill();
 }
 
-function draw( elm ){
+/**
+ * Draws a rectangle using the properties of passed argument Object that must contain x, y, width and height fields declared and initialized
+ * @param {Object} elm - element to draw a rectangle using its fields values {x: number, y: number , width: number, height: number}
+ */
+function draw(elm) {
 	ctx.beginPath();
 	ctx.rect(elm.x, elm.y, elm.width, elm.height);
 	ctx.stroke();
 }
 
-function isColliding( obj1, obj2){
+/**
+ * Draws a vertical visual reference Line as width as the canvas and at y = @GROUND_POS
+ */
+function drawGroundLine() {
+	ctx.beginPath();
+	ctx.moveTo(0, GROUND_POS);
+	ctx.lineTo(cnv.width, GROUND_POS);
+	ctx.stroke();
+}
+
+/**
+ * Returns true or false if the arguments objects in game collide
+ * @param {{}} obj1 - 
+ * @param {{}} obj2 -
+ * @returns {boolean} - 
+ */
+function isColliding(obj1, obj2) {
 	return ((obj2.x - obj1.x + 10) < (obj1.width - 40) && (obj1.x - obj2.x + 40) < (obj2.width - 10)) && ((obj2.y - obj1.y + 10) < obj1.height);
 }
 
 drawLoop();
 updateLoop();
-listenForTouches(player);
-//listenForKeys(player);
+//listenForTouches(player);
+listenForKeys(player);
 
+/**
+ * Adds all necessary touch listener events for user interaction with the GamePad Elements
+ * @param {{}} character - instance of the character the user is playing
+ */
 function listenForTouches(character) {
-  document.getElementById("left-pad").addEventListener("touchstart", function (e) {
-	  e.preventDefault();
-	  character.isMovingLeft = true;
-  });
-
-  document.getElementById("left-pad").addEventListener("touchend", function (e) {
-	e.preventDefault();
-    character.isMovingLeft = false;
-  });
-
-  document.getElementById("right-pad").addEventListener("touchstart", function (e) {
-	  e.preventDefault();
-	  character.isMovingRight = true;
-  });
-
-  document.getElementById("right-pad").addEventListener("touchend", function (e) {
-	e.preventDefault();
-    character.isMovingRight = false;
-  });
-
-  document.getElementById("jump-pad").addEventListener("touchstart", function (e) {
-	e.preventDefault();
-	character.allowJump = character.y >= GROUND_POS ? true : false;
-	if(character.allowJump){
-		log.innerHTML += `<br>Allow Jump Request<br>`;
-		window.requestAnimationFrame(updatePlayer);
-	}else{
-		log.innerHTML = `<br>Reject Jump Request<br>`;
-	}
-  });
-
-  document.getElementById("trow-pad").addEventListener("touchstart", function (e) {
-   	e.preventDefault();
-  });
+	document.getElementById("left-pad").addEventListener("touchstart", function (e) {
+		e.preventDefault();
+		character.isMovingLeft = true;
+	});
+	document.getElementById("left-pad").addEventListener("touchend", function (e) {
+		e.preventDefault();
+		character.isMovingLeft = false;
+	});
+	document.getElementById("right-pad").addEventListener("touchstart", function (e) {
+		e.preventDefault();
+		character.isMovingRight = true;
+	});
+	document.getElementById("right-pad").addEventListener("touchend", function (e) {
+		e.preventDefault();
+		character.isMovingRight = false;
+	});
+	document.getElementById("jump-pad").addEventListener("touchstart", function (e) {
+		e.preventDefault();
+		if (!character.jumpInProgess)
+			window.requestAnimationFrame(updatePlayer);
+	});
+	document.getElementById("trow-pad").addEventListener("touchstart", function (e) {
+		e.preventDefault();
+	});
 }
 
+/**
+ * Adds all KeyDown and KeyUp Listener events for the user interaction with the Keyboard
+ * @param {{}} character - instance of the character the user is playing
+ */
 function listenForKeys(character) {
 	document.addEventListener("keydown", function (e) {
-	  const k = e.key;
-	  if (e.code == "Space") {
-
-	  }
-
-	  if (k == "ArrowRight") {
-		character.isMovingRight = true;
-		character.direction = RIGHT;
-	  }
-	  if (k == "ArrowLeft") {
-		character.isMovingLeft = true;
-		character.direction = LEFT;
-	  }
-
-	  if (k == "d") {
-		
-	  }
+		const k = e.key;
+		if (e.code == "Space" && !character.jumpInProgess) {
+			window.requestAnimationFrame(jump);
+		}
+		if (k == "ArrowRight" && !character.isMovingRight) {
+			character.isMovingRight = true;
+			character.direction = RIGHT;
+			player.requestMoveRight = window.requestAnimationFrame(moveRight);
+		}
+		if (k == "ArrowLeft" && !character.isMovingLeft) {
+			character.isMovingLeft = true;
+			character.direction = LEFT;
+			player.requestMoveLeft = window.requestAnimationFrame(moveLeft);
+		}
+		if (k == "d" && !character.throwInProgress) {
+			character.bottle.initY = character.y;
+			window.requestAnimationFrame(throwBottle);
+		}
 	});
-
 	document.addEventListener("keyup", function (e) {
-	  const k = e.key;
-	  if (k == "ArrowRight") {
-		character.isMovingRight = false;
-	  }
-	  if (k == "ArrowLeft") {
-		character.isMovingLeft = false;
-	  }
+		const k = e.key;
+		if (k == "ArrowRight" && character.isMovingRight) {
+			character.isMovingRight = false;
+			window.cancelAnimationFrame(player.requestMoveRight);
+		}
+		if (k == "ArrowLeft" && character.isMovingLeft) {
+			character.isMovingLeft = false;
+			window.cancelAnimationFrame(player.requestMoveLeft);
+		}
 	});
-  }
+}
